89749401bdf4fe54fe0297dc9e2aad6baee1e281
David <david.chui@outlook.com>
2025-09-25 15:13:58 -0600

    Convert text to vectors and sotre in database

--- apps/pi-ai/initialize.bash
+++ apps/pi-ai/initialize.bash
@@ -2,3 +2,6 @@ conda create --name rag_chatbot python=3.11 -y
 conda activate rag_chatbot
 
 pip install langchain chromadb pypdf sentence-transformers jupyterlab unstructured libmagic
+
+#later after ingest.py runs and finishes in the same directory cd D:\Repos\PiCluster\apps\pi-ai
+python ingest.py
